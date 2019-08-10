3fdb21122fee34935d403e63b197651b2a59ddb0
Volodymyr Rykhva <volodymyr.rykhva@sigma.software>
2019-08-10 08:35:22 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Questionnaire());
+
+class Questionnaire extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
